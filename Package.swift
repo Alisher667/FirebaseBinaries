87b9c2e7e684fa527caa95b0606b34259fc82a6f
Alisher <alisher7@me.com>
2022-12-11 11:53:23 +0600

    version changed

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FirebaseBinaries",
     platforms: [.macOS(.v10_13),
-                .iOS(.v11)],
+                .iOS(.v12)],
     products: [
         .library(
             name: "FirebaseBinaries",
