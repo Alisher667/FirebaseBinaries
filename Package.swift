d82b3851a831b12122dc98ee096080f5a5b3aaf5
Alisher <alisher7@me.com>
2024-07-08 00:46:02 +0500

    analytics and messaging

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "FirebaseBinaries",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
             targets: [
                 "FirebaseBinaries",
                 "FirebaseAnalytics",
-                "FirebaseAnalyticsSwift",
+                "FBLPromises",
                 "FirebaseCore",
                 "FirebaseCoreInternal",
                 "FirebaseInstallations",
@@ -23,23 +23,14 @@ let package = Package(
                 "GoogleAppMeasurementIdentitySupport",
                 "GoogleUtilities",
                 "nanopb",
-                "FirebaseCrashlytics",
-                "GoogleDataTransport",
                 "FirebaseMessaging",
-                "FirebaseDynamicLinks",
-                "FirebaseRemoteConfig",
-                "FirebaseABTesting",
-                "FirebaseRemoteConfigSwift",
-                "FirebaseSharedSwift",
-                "FirebaseCoreExtension",
-                "FirebaseSessions",
-                "FBLPromises"
+                "GoogleDataTransport"
             ]
         ),
     ],
     targets: [
         .binaryTarget(name: "FirebaseAnalytics", path: "Frameworks/FirebaseAnalytics/FirebaseAnalytics.xcframework"),
-        .binaryTarget(name: "FirebaseAnalyticsSwift", path: "Frameworks/FirebaseAnalytics/FirebaseAnalyticsSwift.xcframework"),
+        .binaryTarget(name: "FBLPromises", path: "Frameworks/FirebaseAnalytics/FBLPromises.xcframework"),
         .binaryTarget(name: "FirebaseCore", path: "Frameworks/FirebaseAnalytics/FirebaseCore.xcframework"),
         .binaryTarget(name: "FirebaseCoreInternal", path: "Frameworks/FirebaseAnalytics/FirebaseCoreInternal.xcframework"),
         .binaryTarget(name: "FirebaseInstallations", path: "Frameworks/FirebaseAnalytics/FirebaseInstallations.xcframework"),
@@ -47,22 +38,13 @@ let package = Package(
         .binaryTarget(name: "GoogleAppMeasurementIdentitySupport", path: "Frameworks/FirebaseAnalytics/GoogleAppMeasurementIdentitySupport.xcframework"),
         .binaryTarget(name: "GoogleUtilities", path: "Frameworks/FirebaseAnalytics/GoogleUtilities.xcframework"),
         .binaryTarget(name: "nanopb", path: "Frameworks/FirebaseAnalytics/nanopb.xcframework"),
-        .binaryTarget(name: "FirebaseCrashlytics", path: "Frameworks/FirebaseCrashlytics/FirebaseCrashlytics.xcframework"),
-        .binaryTarget(name: "GoogleDataTransport", path: "Frameworks/FirebaseCrashlytics/GoogleDataTransport.xcframework"),
         .binaryTarget(name: "FirebaseMessaging", path: "Frameworks/FirebaseMessaging/FirebaseMessaging.xcframework"),
-        .binaryTarget(name: "FirebaseDynamicLinks", path: "Frameworks/FirebaseDynamicLinks/FirebaseDynamicLinks.xcframework"),
-        .binaryTarget(name: "FirebaseRemoteConfig", path: "Frameworks/FirebaseRemoteConfig/FirebaseRemoteConfig.xcframework"),
-        .binaryTarget(name: "FirebaseABTesting", path: "Frameworks/FirebaseRemoteConfig/FirebaseABTesting.xcframework"),
-        .binaryTarget(name: "FirebaseRemoteConfigSwift", path: "Frameworks/FirebaseRemoteConfig/FirebaseRemoteConfigSwift.xcframework"),
-        .binaryTarget(name: "FirebaseSharedSwift", path: "Frameworks/FirebaseRemoteConfig/FirebaseSharedSwift.xcframework"),
-        .binaryTarget(name: "FirebaseCoreExtension", path: "Frameworks/FirebaseCrashlytics/FirebaseCoreExtension.xcframework"),
-        .binaryTarget(name: "FirebaseSessions", path: "Frameworks/FirebaseCrashlytics/FirebaseSessions.xcframework"),
-        .binaryTarget(name: "FBLPromises", path: "Frameworks/FirebaseAnalytics/FBLPromises.xcframework"),
+        .binaryTarget(name: "GoogleDataTransport", path: "Frameworks/FirebaseMessaging/GoogleDataTransport.xcframework"),
         .target(
             name: "FirebaseBinaries",
             dependencies: [
                 "FirebaseAnalytics",
-                "FirebaseAnalyticsSwift",
+                "FBLPromises",
                 "FirebaseCore",
                 "FirebaseCoreInternal",
                 "FirebaseInstallations",
@@ -70,17 +52,8 @@ let package = Package(
                 "GoogleAppMeasurementIdentitySupport",
                 "GoogleUtilities",
                 "nanopb",
-                "FirebaseCrashlytics",
-                "GoogleDataTransport",
                 "FirebaseMessaging",
-                "FirebaseDynamicLinks",
-                "FirebaseRemoteConfig",
-                "FirebaseABTesting",
-                "FirebaseRemoteConfigSwift",
-                "FirebaseSharedSwift",
-                "FirebaseCoreExtension",
-                "FirebaseSessions",
-                "FBLPromises"
+                "GoogleDataTransport"
             ]
         )
     ]
